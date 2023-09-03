77cceb343c8ed5b8cf86fad0ca1711604ada190b
mykeels <mykehell123@gmail.com>
2023-09-03 09:36:31 -0400

    feat: add verified mf

---
+++ mf-verified/craco.config.js
@@ -0,0 +1,31 @@
+const overrideWebpackConfig = require("./config-overrides");
+
+const config = {
+  reactScriptsVersion: "react-scripts" /* (default value) */,
+  style: {
+    css: {
+      loaderOptions: cssLoaderOptions => {
+        cssLoaderOptions.url = false;
+        return cssLoaderOptions;
+      }
+    }
+  },
+  webpack: {
+    configure: (webpackConfig, { env }) => {
+      if (process.env.WEBPACK_NODE_ENV !== "production") {
+        webpackConfig.optimization.minimize = false;
+        webpackConfig.optimization.mergeDuplicateChunks = true;
+        webpackConfig.optimization.chunkIds = "named";
+        webpackConfig.optimization.flagIncludedChunks = true;
+        webpackConfig.optimization.concatenateModules = true;
+      }
+      return overrideWebpackConfig(webpackConfig, env);
+    }
+  },
+  plugins: [],
+  typescript: {
+    enableTypeChecking: true
+  }
+}
+
+module.exports = config;
---
+++ mf-verified/src/index.ts
@@ -0,0 +1,1 @@
+import("./nav-verified.tsx");
