5682335cd0f2fd7937d23608588482db0cee4fa0
mykeels <mykehell123@gmail.com>
2023-09-03 09:31:39 -0400

    feat: add communities

---
+++ mf-communities/config-overrides.js
@@ -0,0 +1,56 @@
+/* config-overrides.js */
+const path = require("path");
+const ModuleFederationPlugin = require("webpack").container
+  .ModuleFederationPlugin;
+
+const pkg = require("./package.json");
+const getVersion = (name) => pkg.dependencies[name] || pkg.peerDependencies[name];
+
+// eslint-disable-next-line
+module.exports = function override(config, env) {
+  config.output.publicPath = "auto";
+
+  config.target = "web";
+  config.entry = ["./src/index.ts"];
+  config.plugins.push(
+    new ModuleFederationPlugin({
+      name: pkg.name,
+      filename: "remoteEntry.js",
+      exposes: {
+        "./nav-communities": "./src/nav-communities.tsx"
+      },
+      shared: {
+        react: {
+          requiredVersion: getVersion("react"),
+          import: "react",
+          shareKey: "react",
+          shareScope: "default",
+          singleton: true
+        },
+        "react-dom": {
+          requiredVersion: getVersion("react-dom"),
+          singleton: true
+        },
+        "react-router-dom": {
+          requiredVersion: getVersion("react-router-dom"),
+          singleton: true
+        },
+        "react-query": {
+          requiredVersion: getVersion("react-query"),
+          singleton: true
+        }
+      }
+    })
+  );
+
+  config.resolve = {
+    fallback: {
+      https: false,
+      http: false,
+      zlib: false,
+      fs: false,
+      path: false
+    }
+  };
+  return config;
+};
---
+++ mf-communities/src/index.ts
@@ -0,0 +1,1 @@
+import("./nav-communities.tsx");
