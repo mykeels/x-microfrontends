59568d180b635b5cfa2fb342a8409d0e617babcc
mykeels <mykehell123@gmail.com>
2023-09-03 09:10:29 -0400

    feat: sort by priority

--- chassis/src/components/Layout/index.tsx
+++ chassis/src/components/Layout/index.tsx
@@ -26,18 +26,15 @@ export const Layout = () => {
             className="flex self-start h-12 w-12 mx-4"
           />
           <ul className="flex flex-col gap-4 list-none">
-            <MicrofrontendSlot className="contents" name="nav:item" />
-            <li className="flex flex-row self-start py-2 px-4 rounded-full tabs-item gap-4 outline outline-teal-300">
-              <svg
-                className="search-icon"
-                viewBox="0 0 24 24"
-                width="24"
-                fill="white"
-              >
-                <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-              </svg>
-              <h1>Explore</h1>
-            </li>
+            <MicrofrontendSlot
+              className="contents"
+              name="nav:item"
+              transform={(slots) =>
+                slots.sort((a, b) => {
+                  return (a.priority || 0) - (b.priority || 0);
+                })
+              }
+            />
             <li className="flex flex-row self-start py-2 px-4 rounded-full tabs-item gap-4 outline outline-yellow-200">
               <svg
                 className="noti-icon"
